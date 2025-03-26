deb82986a7283acf8c8bda319ec098cc699c39b0
rominer_11 <augustcho2000@gmail.com>
2025-03-25 22:38:34 -0700

    Board class created

---
+++ board.java
@@ -0,0 +1,18 @@
+/**
+ * Board class
+ * @author August Cho
+ * @version 2025-03-25
+ */
+public class board
+{
+	private int a;
+	private int b;
+	private int k;
+
+	public board(a, b, k)
+	{
+		this.a = a;
+		this.b = b;
+		this.k = k;
+	}
+}
