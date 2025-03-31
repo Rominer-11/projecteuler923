81a3978408aaa0541784362848cd471fc62644f7
rominer_11 <augustcho2000@gmail.com>
2025-03-31 12:00:26 -0700

    made broken simulator

--- board.java
+++ board.java
@@ -8,11 +8,47 @@ public class board
 	private int a;
 	private int b;
 	private int k;
+	private char[][] board;
 
-	public board(a, b, k)
+	public board(int a, int b, int k)
 	{
 		this.a = a;
 		this.b = b;
 		this.k = k;
+
+		this.board = new char[a+1][b+1];
+
+		this.init();
+	}
+
+	/**
+	 * Initializes board to staircase form
+	 */
+	public void init()
+	{
+		int stepWidth = b / k;
+		int stepHeight = a / k;
+
+		for (int step = 0; step < k; step++)
+		{
+			for (int row = 0; row < stepHeight; row++)
+			{
+				printBoard();
+				board[row][board.length - (step * stepWidth) - 1] = 'o';
+			}
+		}
+	}
+
+	public void printBoard()
+	{
+		for (int row = 0; row < board.length; row++)
+		{
+			for (int col = 0; col < board[0].length; col++)
+			{
+				System.out.print(board[row][col] + " ");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
 	}
 }
---
+++ runner.java
@@ -0,0 +1,7 @@
+public class runner
+{
+	public static void main(String[] args)
+	{
+		board b = new board(5, 5, 1);
+	}
+}
