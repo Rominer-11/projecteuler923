8cdfaaf8dfc7537b448e42c51d70b0ee09682b38
rominer_11 <augustcho2000@gmail.com>
2025-04-07 20:02:07 -0700

    Finished the board class

--- board.java
+++ board.java
@@ -8,7 +8,7 @@ public class board
 	private int a;
 	private int b;
 	private int k;
-	private char[][] board;
+	private int[][] board;
 
 	public board(int a, int b, int k)
 	{
@@ -16,9 +16,11 @@ public class board
 		this.b = b;
 		this.k = k;
 
-		this.board = new char[a+1][b+1];
+		this.board = new int[a * k][b * k];
 
 		this.init();
+
+		this.printBoard();
 	}
 
 	/**
@@ -26,15 +28,17 @@ public class board
 	 */
 	public void init()
 	{
-		int stepWidth = b / k;
-		int stepHeight = a / k;
+		int step = 0;
 
-		for (int step = 0; step < k; step++)
+		for (int row = 0; row < board.length; row++)
 		{
-			for (int row = 0; row < stepHeight; row++)
+			for (int col = 0; col < (board[0].length - (step * b)); col++)
 			{
-				printBoard();
-				board[row][board.length - (step * stepWidth) - 1] = 'o';
+				board[row][col] = 1;
+			}
+			if ((row + 1) % a == 0)
+			{
+				step++;
 			}
 		}
 	}
--- runner.java
+++ runner.java
@@ -2,6 +2,9 @@ public class runner
 {
 	public static void main(String[] args)
 	{
-		board b = new board(5, 5, 1);
+		board a = new board(7, 2, 4);
+		board b = new board(3, 3, 3);
+		board c = new board(1, 5, 1);
+		board d = new board(6, 1, 2);
 	}
 }
